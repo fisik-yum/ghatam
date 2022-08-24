f5e52d696df0aa5717355fbbeb428ec9c6e4f1c5
fisik-yum <govind.sriram@aol.com>
2022-08-24 14:09:08 -0700

    added reply feature

--- main.go
+++ main.go
@@ -77,10 +77,11 @@ func main() {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	//basic functionality
 	if s.State.User.ID == m.Author.ID {
 		return
-	}
-	if m.Author.ID != ownerID {
+	} else if m.Author.ID != ownerID {
 		c, err := s.Channel(m.ChannelID)
 		check(err)
 		if c.Type != discordgo.ChannelTypeDM { //only allow DM channels
@@ -90,14 +91,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			create_bind(m.Author.ID, defaultChannel)
 		}
 		cID := find_bind(m.Author.ID)
-		s.ChannelMessageSend(cID, ("**" + m.Author.Username + "<" + m.Author.ID + ">" + ":** " + m.Content))
+		s.ChannelMessageSend(cID, (m.Author.ID + " **" + m.Author.Username + "#" + m.Author.Discriminator + ":** " + m.Content))
 		return
 	}
 
-	//command handling, modular prefixes
+	//command handling, modular prefixes, accessible to only the owner
 	cmd := trim_index(m.Content, 0)
-	if m.Author.ID == ownerID && strings.HasPrefix(cmd, prefix) {
-		if strings.HasPrefix(cmd, prefix+"bind") { //bind command. add functions to modify binds, otherwise useless.
+	if m.Author.ID == ownerID && m.Type != discordgo.MessageTypeReply {
+		switch strings.HasPrefix(cmd, prefix) {
+
+		case strings.HasPrefix(cmd, prefix+"bind "): //also modifies existing binds
 			bindUS := trim_index(m.Content, 1)
 			bindCID := trim_index(m.Content, 2)
 			if find_bind(bindUS) == "" {
@@ -106,13 +109,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			modify_bind(bindUS, bindCID)
 			s.ChannelMessageSend(m.ChannelID, "rebound user "+bindUS)
 			return
-		}
-		if strings.HasPrefix(cmd, prefix+"listbinds") {
+
+		case strings.HasPrefix(cmd, prefix+"listbinds"):
 			fmt.Println(bindings)
 			save_bindings()
 			return
-		}
-		if strings.HasPrefix(cmd, prefix+"reset") {
+
+		case strings.HasPrefix(cmd, prefix+"reset "):
 			bindUS := trim_index(m.Content, 1)
 			if find_bind(bindUS) == "" {
 				create_bind(bindUS, defaultChannel)
@@ -120,13 +123,32 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			modify_bind(bindUS, defaultChannel)
 			s.ChannelMessageSend(m.ChannelID, "reset bind for user "+bindUS)
 			return
-		}
-		if strings.HasPrefix(cmd, prefix+"savebinds") {
+
+		case strings.HasPrefix(cmd, prefix+"savebinds"):
 			save_bindings()
-		}
-		if strings.HasPrefix(cmd, prefix+"info") {
+			return
+
+		case strings.HasPrefix(cmd, prefix+"info"):
 			s.ChannelMessageSend(m.ChannelID, ("` kavaca built with discordgo " + discordgo.VERSION + "`"))
 			return
+
+		default:
+			s.ChannelMessageSend(m.ChannelID, "Invalid Command")
+			return
 		}
+	} else if m.Author.ID == ownerID && m.Type == discordgo.MessageTypeReply {
+		im, err := s.ChannelMessage(m.MessageReference.ChannelID, m.MessageReference.MessageID)
+		check(err)
+		truncate := len(im.Author.ID+" **"+im.Author.Username+"#"+im.Author.Discriminator+":** ") - 1
+		if (len(im.Content) < truncate-1) || im.Author.ID != s.State.User.ID {
+			s.ChannelMessageSend(m.ChannelID, "An error occurred")
+			return
+		}
+		originalID := im.Content[:18]
+		c, err := s.UserChannelCreate(originalID)
+		check(err)
+		rmessagecontent := fmt.Sprintf("`Reply to message sent @ %s :` *%s*\n%s", string(im.Timestamp)[:19], im.Content[truncate:], m.Content)
+		s.ChannelMessageSend(c.ID, rmessagecontent)
+		return
 	}
 }
